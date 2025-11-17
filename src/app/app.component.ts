bdc69796766a0ad0f16a7cecd1e63f10c9936185
Ehab Ahmed <ehab.ahmed0202@gmail.com>
2025-11-17 14:43:22 +0200

    initializing e-commerce-front app

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { afterRender, Component, computed, effect, Signal, signal, ViewChild } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+// here you have to import any component you want to use
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  NgForm,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  CommonModule,
+  NgFor,
+  NgIf,
+  NgSwitch,
+  NgSwitchCase,
+  NgSwitchDefault,
+} from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    CommonModule,
+    //this for ngModel
+    FormsModule,
+    NgIf,
+    NgFor,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title: string = 'counter app';
+ 
+}
+
+
