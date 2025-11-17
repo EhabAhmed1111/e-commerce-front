552977e9599eeac1681a46206f0665fff820ed98
Ehab Ahmed <ehab.ahmed0202@gmail.com>
2025-11-17 18:23:57 +0200

    creating header and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,22 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  
+  {
+    path: "header",
+    component: HeaderComponent
+  },{
+    path: "",
+    component: HomeComponent
+  },{
+    path: "signup",
+    loadComponent: () => import('./signup/signup.component').then((c)=>c.SignupComponent)
+  },{
+    path: "topseller",
+    loadComponent: () => import('./topseller/topseller.component').then((c)=>c.TopsellerComponent)
+  },{
+    path: "cart",
+    loadComponent: () => import('./cart/cart.component').then((c)=>c.CartComponent)
+  }
 ];
---
+++ src/app/topseller/topseller.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topseller',
+  imports: [],
+  templateUrl: './topseller.component.html',
+  styleUrl: './topseller.component.scss'
+})
+export class TopsellerComponent {
+
+}
