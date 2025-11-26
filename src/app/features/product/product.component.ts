c95c2fe96920f90e1a56e0c12c9644fdeae46ce6
Ehab Ahmed <ehab.ahmed0202@gmail.com>
2025-11-26 15:33:47 +0200

    adding input and select filter

--- src/app/features/product/product.component.ts
+++ src/app/features/product/product.component.ts
@@ -1,23 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal, Signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { ProductService } from '../../core/services/product/product.service';
 import { ProductsResponse, Products, ProductsForShow, CategoriesResponse, Category } from '../../core/models/data';
 import { CategoryService } from '../../core/services/category/category.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FormsModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
 })
 export class ProductComponent {
   /*---- this will not be modified -----*/
   products: ProductsForShow[] = [];
+  filteredProducts: ProductsForShow[] = [];
   categories: Category[] = [];
+  /** shoud it become signal?? */
+  searchText: string = "";
+  category: string = "";
 
-
-  constructor(private productService: ProductService, private categoryService: CategoryService) { }
+  constructor(private productService: ProductService, private categoryService: CategoryService) {
+  }
 
   ngOnInit() {
     this.productService
@@ -34,12 +39,49 @@ export class ProductComponent {
           };
         });
       });
+
   }
 
   getAllCategories() {
     this.categoryService.getAllCategories().subscribe((category: CategoriesResponse): void => {
       this.categories = category.data;
-    })
+    });
   }
 
+  onSearchChange(event: Event) {
+    this.searchText = (event.target as HTMLInputElement).value;
+    this.filteredProducts = this.filterProductsBySearchText(this.searchText);
+  }
+
+  // if there is no product out of filter then the original products will be shown
+  onSelectCategoryChange(event: Event) {
+    this.category = (event.target as HTMLSelectElement).value;
+    /* if there are search text i should filter it 2 time first by category then by search text */
+    if (this.searchText.length > 0) {
+      /* here we apply search text only */
+      if (this.category === "All") {
+        this.filteredProducts = this.filterProductsBySearchText(this.searchText);
+      }
+      this.filteredProducts = this.filterProductsByCategoryAndSearchText(this.category, this.searchText);
+    } else {
+      this.filteredProducts = this.filterProductsByCategory(this.category);
+    }
+  }
+
+  private filterProductsByCategory(categoryName: string): ProductsForShow[] {
+    return this.products.filter((product: ProductsForShow) => {
+      return product.categoryName === categoryName;
+    });
+  }
+  private filterProductsBySearchText(productName: string): ProductsForShow[] {
+    return this.products.filter((product: ProductsForShow) => {
+      return product.productName.toLowerCase().includes(productName.toLocaleLowerCase());
+    });
+  }
+
+  private filterProductsByCategoryAndSearchText(categoryName: string, productName: string): ProductsForShow[] {
+    return this.products.filter((product: ProductsForShow) => {
+      return product.categoryName === categoryName && product.productName.toLowerCase().includes(productName.toLocaleLowerCase());
+    });
+  }
 }
